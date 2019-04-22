2fdc9280f4be9c6be31bc8d7a1cb90186be9b17d
Seth Watkins <dontpanic@Seths-MacBook-Pro.local>
2019-04-22 09:33:47 -0500

    fix: Ensure errors are cleared onChange

--- src/ContactForm/index.js
+++ src/ContactForm/index.js
@@ -4,19 +4,28 @@ import {IconContext} from "react-icons/lib";
 
 export const ContactForm = (props) => {
 
-    const {nameError, emailError, messageError, validator} = useContactFormError()
+    const {nameError, emailError, messageError, validator, clearError} = useContactFormError()
     const {nameValue, emailValue, messageValue, setNameValue, setEmailValue, setMessageValue} = useContactFormValues()
 
     return(
         <form className='contact-form' onSubmit={(e) => e.preventDefault()}>
             <label>Name:</label>
-            <input type='text' placeholder='name' value={nameValue} onChange={(e) => setNameValue(e.target.value)} onBlur={() => {validator(nameValue, 'name')}}/>
+            <input type='text' placeholder='name' value={nameValue} onChange={(e) => {
+                setNameValue(e.target.value)
+                nameError && clearError('name')
+            }} onBlur={() => {validator(nameValue, 'name')}}/>
             {nameError && nameError}
             <label>Email:</label>
-            <input type='text' placeholder='email' value={emailValue} onChange={(e) => setEmailValue(e.target.value)} onBlur={() => {validator(emailValue, 'email')}}/>
+            <input type='text' placeholder='email' value={emailValue} onChange={(e) => {
+                setEmailValue(e.target.value)
+                emailError && clearError('email')
+            }} onBlur={() => {validator(emailValue, 'email')}}/>
             {emailError && emailError}
             <label>Message:</label>
-            <textarea value={messageValue} onChange={(e) => setMessageValue(e.target.value)} onBlur={() => {validator(messageValue, 'message')}}/>
+            <textarea value={messageValue} onChange={(e) => {
+                setMessageValue(e.target.value)
+                messageError && clearError('message')
+            }} onBlur={() => {validator(messageValue, 'message')}}/>
             {messageError && messageError}
             <div className='submit-container'>
                 <IconContext.Provider value={{color: '#ffeb7f', className: 'spinner', size: '1.25rem'}}>
@@ -42,10 +51,21 @@ export const useContactFormError = () => {
         }
 
         const {set, msg} = typeMap[type]
-        value.length < 1 && set(msg)
+        value.length ? set('') : set(msg)
     }
 
-    return {nameError, emailError, messageError, validator}
+    const clearError = (type) => {
+        const typeMap = {
+            name: setNameError,
+            email: setEmailError,
+            message: setMessageError
+        }
+
+        typeMap[type]('')
+
+    }
+
+    return {nameError, emailError, messageError, validator, clearError}
 }
 
 export const useContactFormValues = () => {
